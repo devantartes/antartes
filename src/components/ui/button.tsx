62209ed957ec310c4a968f3b1a5e4def56ada880
Mumuni Abdulazeez <mumuniazeez99@gmail.com>
2026-01-06 02:29:46 +0100

    feat: allow ButtonBG to accept and spread standard button HTML attributes

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,13 +1,17 @@
+import type { ButtonHTMLAttributes } from "react";
+
 export function ButtonBG({
   children,
   className,
+  ...props
 }: {
   children: string;
   className?: string;
-}) {
+} & ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       className={`bg-primary cursor-pointer text-black px-4 py-2 rounded-2xl text-[16px] ${className}`}
+      {...props}
     >
       {children}
     </button>
